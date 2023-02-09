5a2f7e739f7172ba98b9964c1696104686c2af3e
Taya Inakova <punkgrrl@yandex.ru>
2023-02-09 19:14:51 +0300

    homework 4 do

--- Homework4.java
+++ Homework4.java
@@ -2,8 +2,7 @@
 2. Реализуйте очередь с помощью LinkedList со следующими методами:
 enqueue() - помещает элемент в конец очереди,
 dequeue() - возвращает первый элемент из очереди и удаляет его,
-first() - возвращает первый элемент из очереди, не удаляя.
-3*. В калькулятор добавьте возможность отменить последнюю операцию. */
+first() - возвращает первый элемент из очереди, не удаляя. */
 
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -12,39 +11,36 @@ import java.util.Queue;
 
 public class Homework4 {
     public static void main(String[] args) {
-        
+
         List<String> example = new LinkedList<>(Arrays.asList("one", "two", "three", "four", "five", "six",
                 "seven", "eight", "nine", "ten", "eleven", "twelve"));
         List<String> test = flipTheList(example);
         printStringList(example);
         printStringList(test);
-        
-        Queue<String> q = new LinkedList<String>();
-        q.add("один");
-        q.add("два");
-        q.add("три");
-        q.add("четыре");
-        q.add("пять");
-        q.add("шесть");
-        q.add("семь");
-        q.add("восемь");
-        q.add("девять");
-        q.add("десять");
-        
-        // System.out.println("Queue header = " + q.element());
-        // System.out.println("Size = " + q.size());
-        // System.out.println("Queue elements ...");
-        
-        
-        enqueue(q, "мама");Object ob;
-        
-        while ((ob = q.poll()) != null) {
-            System.out.print(ob + " ");
-        }
-        
+
+        Queue<String> example01 = new LinkedList<String>();
+        example01.add("один");
+        example01.add("два");
+        example01.add("три");
+        example01.add("четыре");
+        example01.add("пять");
+        example01.add("шесть");
+        example01.add("семь");
+        example01.add("восемь");
+        example01.add("девять");
+        example01.add("десять");
+
+        enqueue(example01, "мама");
+        String a = dequeue(example01);
+        String b = first(example01);
+
+        System.out.println(a);
+        System.out.println(b);
+        printStringQueue(example01);
+
     }
-    
-// Переворачвает список
+
+    // Переворачивает список
     public static List<String> flipTheList(List<String> something) {
 
         List<String> newList = new LinkedList<>();
@@ -54,7 +50,7 @@ public class Homework4 {
         return newList;
     }
 
-// Печатает список строк в консоль
+    // Печатает список строк в консоль
     public static void printStringList(List<String> n) {
 
         for (String i : n) {
@@ -64,26 +60,37 @@ public class Homework4 {
 
     }
 
-// Помещает элемент в конец очереди,
+    // Печатает очередь строк в консоль
+    public static void printStringQueue(Queue<String> u) {
+
+        Object ob;
+        while ((ob = u.poll()) != null) {
+            System.out.print(ob + " ");
+        }
+        
+        System.out.println();
+
+    }
+
+    // Помещает элемент в конец очереди,
     public static void enqueue(Queue<String> a, String el) {
 
         a.add(el);
 
     }
 
-// Возвращает первый элемент из очереди и удаляет его
+    // Возвращает первый элемент из очереди и удаляет его
     public static String dequeue(Queue<String> b) {
 
-        String firstElement = b.element();
-        b.remove();
+        String firstElement = b.poll();
 
         return firstElement;
 
     }
 
-// возвращает первый элемент из очереди, не удаляя
+    // возвращает первый элемент из очереди, не удаляя
     public static String first(Queue<String> c) {
-        
+
         String firstElement = c.element();
 
 
